2dad7d5b17b6f220c5af12d7de196f3c535527b3
Flávio Porfírio <flavioporfirio86@gmail.com>
2024-02-27 20:42:00 -0300

    fix: project component overflow

--- src/components/Project/ProjectList.js
+++ src/components/Project/ProjectList.js
@@ -61,19 +61,4 @@ export const projectList = [
     html: true,
     css: true,
   },
-  {
-    id: uuidv4(),
-    title: "Financial",
-    projectInfo:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ab vel quibusdam quia. Ipsum perferendis earum officia eveniet non fuga, debitis at eum nemo consectetur dignissimos laboriosam cum veniam quae.",
-    tech: ["javascript, css, react, html"],
-    image: "./imgs/project-imgs/financial-control.png",
-    siteLink: "https://financial-control-flavioporfirio86.netlify.app/",
-    repositoryLink: "https://github.com/flavioporfirio/financial-control",
-    youtubeLink: "https://www.youtube.com/channel/UC6ewHz-YN0KpRCWoAH-sdMA",
-    react: false,
-    js: true,
-    html: true,
-    css: true,
-  },
 ];
